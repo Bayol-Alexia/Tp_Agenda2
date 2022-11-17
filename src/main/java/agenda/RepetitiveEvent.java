2ba41e369e89035e04bcce8769778ad765a434e7
abayol <alexia.bayol@etud.univ-jfc.fr>
2022-11-17 11:09:05 +0100

    Premier commit Alexia

--- src/main/java/agenda/RepetitiveEvent.java
+++ src/main/java/agenda/RepetitiveEvent.java
@@ -21,10 +21,13 @@ public class RepetitiveEvent extends Event {
      * <LI>ChronoUnit.MONTHS for monthly repetitions</LI>
      * </UL>
      */
+    protected ChronoUnit frequency;
+    protected ArrayList<LocalDate> exceptionsDates = new ArrayList<LocalDate>();
+
+
     public RepetitiveEvent(String title, LocalDateTime start, Duration duration, ChronoUnit frequency) {
         super(title, start, duration);
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        this.frequency = frequency;
     }
 
     /**
@@ -33,17 +36,37 @@ public class RepetitiveEvent extends Event {
      * @param date the event will not occur at this date
      */
     public void addException(LocalDate date) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        exceptionsDates.add(date);
+    }
+
+    public ArrayList<LocalDate> getExceptionsDates() {
+        return exceptionsDates;
+    }
+
+
+
+    public boolean isInDay(LocalDate aDay){
+        //initialisation de dateSup à la date de début de l'évènement
+        LocalDate dateSup = getStart().toLocalDate();
+        //vérification que le jour choisi se trouve après le jour de début
+        if (aDay.isAfter(dateSup) || aDay.equals(dateSup)){
+            while (aDay.isAfter(dateSup) || aDay.equals(dateSup)){
+                if (dateSup.equals(aDay) && (!exceptionsDates.contains(aDay))){
+                    return true;
+                }
+                dateSup=dateSup.plus(1, frequency);  
+            }
+        }
+        return false;
     }
 
     /**
      *
      * @return the type of repetition
      */
+
     public ChronoUnit getFrequency() {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");    
+        return frequency;   
     }
 
 }
